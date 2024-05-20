2d8cf26f63ecb6806872bae03b220a214f3f02d3
Sayeed613 <sayeedahmed90082@gmail.com>
2024-05-21 00:15:28 +0530

    done

---
+++ a3p3/src/components/RightSection.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { BulbContext } from "../context/BulbContextProvider";
+
+export default function RightSection() {
+  // Use useContext to get isOn, switchOn, and switchOff from BulbContext
+  const { isOn, switchOn, switchOff } = useContext(BulbContext);
+
+  return (
+    <div className="right-section">
+      <p>Right Section ( Bulb )</p>
+      <div className="light-bulb-container">
+        {/* Apply the class based on the isOn state */}
+        <div className={`light-bulb ${isOn ? "on" : "off"}`}></div>
+        <button onClick={switchOn}>SWITCH ON</button>
+        <button onClick={switchOff}>SWITCH OFF</button>
+      </div>
+    </div>
+  );
+}
---
+++ a3p4/src/pages/Fees.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Fees() {
+  return <h1>Fees page</h1>
+}
+
+export default Fees
